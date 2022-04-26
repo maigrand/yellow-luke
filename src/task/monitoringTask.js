21799d3ef2ab177e3b459aaa4bfc8661d2ad5294
mai <mkmaxz48@gmail.com>
2022-04-26 13:28:56 +0300

    feat: Дополнить monitoring embed; добавить игроков на сервере

--- src/task/monitoringTask.js
+++ src/task/monitoringTask.js
@@ -3,6 +3,10 @@ import {getStatusSmart} from 'jka-core'
 import {MessageEmbed} from 'discord.js'
 import {client} from '../client.js'
 import Monitoring from '../model/Monitoring.js'
+import fs from 'fs'
+import path from 'path'
+
+const mapUrl = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), './src/mapUrl.json')))
 
 export const task = cron.schedule('*/10 * * * * *', async () => {
     await update()
@@ -18,26 +22,62 @@ async function update() {
                 const guild = client.guilds.cache.get(mon.guildId)
                 const channel = await guild.channels.fetch(mon.channelId)
                 const message = await channel.messages.fetch(mon.messageId)
-                const jkaResponse = await getStatusSmart(mon.address)
+                let jkaResponse
 
-                let embed = new MessageEmbed()
+                try {
+                    jkaResponse = await getStatusSmart(mon.address)
+                } catch (e) {
+                    const embed = new MessageEmbed().setTitle(`\uD83D\uDEAB ${mon.name}`)
+                    await message.edit({embeds: [embed]})
+                    continue
+                }
+
                 if (jkaResponse.clients.length === 0) {
                     const emoteOnline = guild.emojis.cache?.find((emoji) => emoji.name === "greendot") === "undefined" ? "\uD83D\uDFE2" : guild.emojis.cache?.find((emoji) => emoji.name === "greendot")
 
-                    embed
+                    const embed = new MessageEmbed()
                         .setTitle(`${emoteOnline} **0/${jkaResponse.cvars.sv_maxclients}** | **${jkaResponse.cvars.g_gametype}** | **${await normalizeJkaString(jkaResponse.cvars.sv_hostname)}**`)
-                        .setFooter({text: `/connect ${mon.address}${mon.password === "undefined" ? undefined : `;${mon.password}`}`,})
+                        .setFooter({text: `/connect ${mon.address}${mon.password === "null" ? `` : `;password ${mon.password}`}`})
                         .setTimestamp(Date.now())
-                } else {
-                    embed
-                        .setAuthor({name: await normalizeJkaString(jkaResponse.cvars.sv_hostname)})
-                        .setTitle(mon.address)
-                        .addField("Map", jkaResponse.cvars.mapname, true)
-                        .addField("Gametype", jkaResponse.cvars.g_gametype, true)
-                        .addField("Fraglimit", jkaResponse.cvars.fraglimit.toString(), true)
-                        .addField("Timelimit", jkaResponse.cvars.timelimit.toString(), true)
-                        .setTimestamp(Date.now())
+                        .setColor([10, 70, 10])
+
+                        await message.edit({embeds: [embed]})
+                        continue
                 }
+
+                let players = ``
+                for (const client of jkaResponse.clients) {
+                    let clientName = client.name
+                    if (clientName.includes("*")) {
+                        clientName = clientName.replaceAll("*", "\\*")
+                    }
+                    if (clientName.includes("_")) {
+                        clientName = clientName.replaceAll("_", "\\_")
+                    }
+                    if (clientName.includes("\"")) {
+                        clientName = clientName.replaceAll("\"", "")
+                    }
+                    if (clientName.includes("|")) {
+                        clientName = clientName.replaceAll("|", "\\|")
+                    }
+                    if (clientName.includes("discord.gg")) {
+                        clientName = clientName.replaceAll(/.+/g, "discord.gg")
+                    }
+                    players+= `${await normalizeJkaString(clientName)} (score: ${client.score})\n`
+                }
+
+                const embed = new MessageEmbed()
+                    .setAuthor({name: await normalizeJkaString(jkaResponse.cvars.sv_hostname)})
+                    .setTitle(mon.address)
+                    .addField("Map", jkaResponse.cvars.mapname, true)
+                    .addField("Gametype", jkaResponse.cvars.g_gametype, true)
+                    .addField("Fraglimit", jkaResponse.cvars.fraglimit.toString(), true)
+                    .addField("Timelimit", jkaResponse.cvars.timelimit.toString(), true)
+                    .addField(`:white_check_mark: Online ${jkaResponse.clients.length}/${jkaResponse.cvars.sv_maxclients}`, players, false)
+                    .setFooter({text: `/connect ${mon.address}${mon.password === "null" ? `` : `;password ${mon.password}`}`})
+                    .setTimestamp(Date.now())
+                    .setColor([10, 160, 10])
+                    .setThumbnail(mapUrl[jkaResponse.cvars.mapname] === undefined ? mapUrl.default : mapUrl[jkaResponse.cvars.mapname])
 
                 await message.edit({embeds: [embed]})
             } catch (e) {
